11aca6f4653defa71cf2c2d3afa46997fd2f1a6a
ORIGINALrak <90960081+ORIGINALrak@users.noreply.github.com>
2024-10-07 10:32:23 +0200

    valaminev

---
+++ molnar-horgos_kristofTriatlon/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualBasic;
+using molnar_horgos_kristofTriatlon;
+using System.Text;
+
+List<Versenyzo> versenyzok = [];
+
+using StreamReader sr = new(path: @"..\..\..\bin\forras.txt", encoding: Encoding.UTF8);
+sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    versenyzok.Add(new(sr.ReadLine()!));
+}
+
+Console.WriteLine($"1. feladat: versenyző fejezte be a versenyt szama: {versenyzok.Count}");
+
+var lnCnt = versenyzok.Count(v => v.csoport == "elit");
+Console.WriteLine($"Elit versenyzők száma: {lnCnt} db");
+
+
+var lnnoi = versenyzok.Where(v => v.neme == "n");
+var lnnoiavg = lnnoi.Average(v => v.szulev);
+Console.WriteLine($"Női versenyzők versenyzők átlag életkora: {DateTime.Now.Year - lnnoiavg:00}");
+
+var lnkerekpar = TimeSpan.FromSeconds(versenyzok.Sum(v => v.kerekpar.TotalSeconds));
+Console.WriteLine($"A versenyzők kerékpározással töltött összideje órában: {lnkerekpar.TotalHours:00}");
+
+
+var lnelit = versenyzok.Where(v => v.csoport == "elit junior");
+var lnelitavg = TimeSpan.FromSeconds(lnelit.Average(v => v.uszas.TotalSeconds));
+
+Console.WriteLine($"Az átlagos úszással eltöltött idő az elit junior kategóriában: {lnelitavg}");
+
+var lnferfi = versenyzok.Where(v => v.neme == "f");
+var lnferfigyoztes = lnferfi.MinBy(v => v.kerekpar.TotalSeconds + v.futas.TotalSeconds + v.uszas.TotalSeconds + v.elsodepo.TotalSeconds + v.masodikdepo.TotalSeconds);
+
+Console.WriteLine($"A férfi győztes: {lnferfigyoztes.nev}");
+
+//var linqByCsoport = versenyzok.GroupBy(v => v.csoport);
+//var linqOrderByCsoport = linqByCsoport.Count(v => v.);
+
+//foreach (var group in linqByCsoport)
+//{
+
+//    Console.WriteLine($"\t{group.Key}");
+//}
---
+++ molnar-horgos_kristofTriatlon/Versenyzo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace molnar_horgos_kristofTriatlon
+{
+    internal class Versenyzo
+    {
+            public string nev { get; set; }
+            public int szulev { get; set; }
+            public int rajtszam { get; set; }
+            public string neme { get; set; }
+            public string csoport { get; set; }
+            public TimeSpan uszas { get; set; }
+            public TimeSpan elsodepo { get; set; }
+            public TimeSpan kerekpar { get; set; }
+            public TimeSpan masodikdepo { get; set; }
+            public TimeSpan futas { get; set; }
+
+        public Versenyzo(string row)
+        {
+            var sor = row.Split(';');
+            nev = (sor[0]);
+            szulev = int.Parse(sor[1]);
+            rajtszam = int.Parse(sor[2]);
+            neme = (sor[3]);
+            csoport = sor[4];
+            uszas = TimeSpan.Parse(sor[5]);
+            elsodepo = TimeSpan.Parse(sor[6]);
+            kerekpar = TimeSpan.Parse(sor[7]);
+            masodikdepo = TimeSpan.Parse(sor[8]);
+            futas = TimeSpan.Parse(sor[9]);
+
+        }
+        //public override string ToString()
+        //{
+        //    return $"{nev} ({szulev} évben született {rajtszam} rajtszama {(neme ? "n" : "m")} {csoport} csoportja {uszas} {elsodepo} {kerekpar}{masodikdepo}{futas})";
+        //}
+    }
+}
